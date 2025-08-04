ba4a4129fae8efc63140febec221703a5709a575
Asik8 <motasembillah84@gmail.com>
2025-08-04 23:35:15 +0600

    First print done

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Hello this is a great start");
+        System.out.println("Now i'll learn other things");
+    }
+}
